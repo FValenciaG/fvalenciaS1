3bcf6d6ac2612d3a0d356ba107712cbac42a6451
FValenciaG <fvalenciag96@hotmail.com>
2024-04-19 20:04:57 -0500

    agrego tarea semana 1, calculo de aporte al IESS

---
+++ fvalenciaS1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace fvalenciaS1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string nombre, apellido;
+        private int edad;
+        private double  aporte;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CalcularBtn_Clicked(object sender, EventArgs e)
+        {
+            nombre = TxtNombre.Text;
+            apellido = TxtApellido.Text;
+            edad = Convert.ToInt32(TxtEdad.Text);
+            aporte = Convert.ToInt32(TxtSalario.Text) * 0.0945;
+
+
+            await DisplayAlert("Alerta", "Bienvenido " + nombre + " " + apellido + "\n" +
+                "tienes " + edad + " años" + "\n" +
+                "Tu aporte mensual es $ " + aporte, "Cerrar");
+
+            TxtNombre.Text = "";
+            TxtApellido.Text = "";
+            TxtEdad.Text = "";
+            TxtSalario.Text = "";
+
+        }
+    }
+
+}
---
+++ fvalenciaS1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace fvalenciaS1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
